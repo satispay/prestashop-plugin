889c0dd51ab99b144642cd16381cdc90345e351b
Stiven Masha <stiven.masha@satispay.com>
2022-03-03 19:17:37 +0100

    Fixes required by Prestashop Validator

--- controllers/front/redirect.php
+++ controllers/front/redirect.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* 2007-2019 PrestaShop
+* 2007-2022 PrestaShop
 *
 * NOTICE OF LICENSE
 *
@@ -19,7 +19,7 @@
 * needs please refer to http://www.prestashop.com for more information.
 *
 *  @author    PrestaShop SA <contact@prestashop.com>
-*  @copyright 2007-2019 PrestaShop SA
+*  @copyright 2007-2022 PrestaShop SA
 *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 */
@@ -32,9 +32,7 @@ class SatispayRedirectModuleFrontController extends ModuleFrontController
         $payment = \SatispayGBusiness\Payment::get($paymentId);
 
         if ($payment->status === 'ACCEPTED') {
-
             for ($i = 0; $i < 6; $i++) {
-
                 $orderId = Order::getOrderByCartId($payment->metadata->cart_id);
                 $order = new Order($orderId);
 
--- processpendingorderscommand.php
+++ processpendingorderscommand.php
@@ -1,10 +1,34 @@
 <?php
+/**
+* 2007-2022 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author    PrestaShop SA <contact@prestashop.com>
+*  @copyright 2007-2022 PrestaShop SA
+*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
 
-require_once __DIR__ . '/../../config/config.inc.php';
-require_once __DIR__ . '/ProcessPendingOrders.php';
-require_once __DIR__ . '/satispay-sdk/lib/Payment.php';
-require_once __DIR__ . '/satispay-sdk/lib/Request.php';
-require_once __DIR__ . '/satispay-sdk/lib/Api.php';
-require_once __DIR__ . '/../../init.php';
+require_once(dirname(__FILE__).'/../../config/config.inc.php');
+require_once(dirname(__FILE__).'/ProcessPendingOrders.php');
+require_once(dirname(__FILE__).'/satispay-sdk/lib/Payment.php');
+require_once(dirname(__FILE__).'/satispay-sdk/lib/Request.php');
+require_once(dirname(__FILE__).'/satispay-sdk/lib/Api.php');
+require_once(dirname(__FILE__).'/../../init.php');
 
 $module = new ProcessPendingOrders();
